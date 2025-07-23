fa51a4e97b3e566d109acaab16d52d543a0478f7
Anika Gupta <anikagupta101@gmail.com>
2025-07-23 14:27:08 -0700

    Initial Commit

---
+++ Swift InteractiveUI/Swift_InteractiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_InteractiveUIApp.swift
+//  Swift InteractiveUI
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Swift_InteractiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
